dd897769fad321c65f5fc5948a7a412bb5b4bbf8
Ashutosh Joshi <ashutxsh.jxshi@gmail.com>
2025-04-29 15:56:42 -0400

    Fix image paths for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/Portfolio' : '',
   images: {
     domains: ['meienberger.dev'],
+    unoptimized: true,
   },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Portfolio/' : '',
 }
 
 module.exports = nextConfig 
